58c4ebef803b6c0309f5a7f74697d18a97f0d44b
Davyson Bitencourt <davybitencourt26@gmail.com>
2025-05-26 15:50:39 -0400

    ATV 14 webdev

---
+++ client/src/pages/profile.jsx
@@ -0,0 +1,14 @@
+import { Outlet, Link } from "react-router-dom";
+import "./Pages.css";
+
+function Profile(){
+    return (
+    <div>
+        <div className="mm">
+            <p> Perfil </p>
+        </div>
+    </div>
+    )
+}
+
+export default Profile;
