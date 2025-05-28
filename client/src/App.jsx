b7f4abf1fc74df299ae1652e05d89d64f54c12f0
Davyson Bitencourt <davybitencourt26@gmail.com>
2025-05-28 15:38:50 -0400

    a bagunça que eu vou ter que oganizar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Produtos from './pages/produtos.jsx'
-import Login from "./pages/login.jsx";
-import CriarLogin from "./pages/criarLogin.jsx";
-import NoPage from "./pages/noPage.jsx"; 
-import Dashboard from "./pages/dashboard.jsx";
+import { BrowserRouter } from "react-router";
+import Routers from "./components/routers/routers.jsx";
+import AuthProvider from "./components/context/auth.jsx";
+import DataProvider from "./components/context/data.jsx";
 
 function App() {
-
   return (
-    <>
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Produtos/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/cadastro" element={<CriarLogin/>}/>
-            <Route path="/dashboard" element={<PrivateRoute> <Dashboard/> </PrivateRoute>}/>
-            <Route path="*" element={<NoPage/>}/>
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
-    </>
-  )
+    <BrowserRouter>
+      <AuthProvider>
+        <DataProvider>
+          <Routers />
+        </DataProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
